46b2b849848f163d8f34589458eddf8540229857
Motongxue <393888412@qq.com>
2023-03-25 00:15:55 +0800

    tree: [226] 翻转二叉树

---
+++ tree/226.翻转二叉树.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=golang
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	invertTree(root.Left)
+	invertTree(root.Right)
+	root.Left, root.Right = root.Right, root.Left
+	return root
+}
+// @lc code=end
+
