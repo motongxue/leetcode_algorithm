c88f356f8ebfa69d2840c73b6af0a57cff46c061
motongxue <393888412@qq.com>
2023-04-03 17:27:56 +0800

    回溯：[78] 子集

---
+++ backtrack/78.子集.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=golang
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+func subsets(nums []int) [][]int {
+	res := [][]int{}
+	var backtracking func(startIdx int, path []int)
+	backtracking = func(startIdx int, path []int) {
+		// 因为每一次的值都是符合要求的值，所以每次进来都进行存值，由于在这，刚好把[]空集处理了
+		tmp := []int{}
+		tmp = append(tmp, path...)
+		res = append(res, tmp)
+		if startIdx == len(nums) {
+			return
+		}
+		for i := startIdx; i < len(nums); i++ {
+			backtracking(i+1, append(path, nums[i]))
+		}
+	}
+	backtracking(0, []int{})
+	return res
+}
+
+// @lc code=end
+
