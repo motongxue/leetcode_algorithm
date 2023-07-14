122c11f062c056cd7d7985d532793b06a1604d21
motongxue <393888412@qq.com>
2023-07-14 11:31:45 +0800

    dp：[1049] 最后一块石头的重量 II

---
+++ dp/1049.最后一块石头的重量-ii.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=1049 lang=golang
+ *
+ * [1049] 最后一块石头的重量 II
+ */
+
+// @lc code=start
+func lastStoneWeightII(stones []int) int {
+    n := len(stones)
+    sum := 0
+    for _, v := range stones {
+        sum += v
+    }
+    // 这里会导致奇数/2被截断
+    bagWeight := sum/2
+    dp := make([]int, bagWeight+1)
+    for i := 0; i < n; i++ {
+        for j := bagWeight; j >= stones[i]; j-- {
+            dp[j] = max(dp[j], dp[j-stones[i]]+stones[i])
+        }
+    }
+    // 实际上就是sum的一半大小的背包容量，能装多少，也就是能被碰撞的数量的一半
+    // dp[bagWeight]*2即为能被碰撞数量，那么剩下的即为剩下的石头
+    return sum-dp[bagWeight]*2
+}
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+// @lc code=end
+
