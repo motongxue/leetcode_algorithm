f12dbdc17a9ab0002a855d9afcb629a0a84ffe9d
motongxue <393888412@qq.com>
2023-07-18 14:09:54 +0800

    回溯：[494] 目标和

--- dp/494.目标和.go
+++ dp/494.目标和.go
@@ -5,34 +5,67 @@
  */
 
 // @lc code=start
+// ========================================
+// 01背包问题
+// func findTargetSumWays(nums []int, target int) int {
+//     // left - right = target
+//     // left + right = sum
+//     // left = (target + sum)/2
+//     sum, n := 0, len(nums)
+//     for _, v := range nums {
+//         sum += v
+//     }
+//     if (target + sum) % 2 == 1 || abs(target) > sum {
+//         return 0
+//     }
+//     bagSize := (target + sum)/2
+//     // dp[j] 表示：填满j（包括j）这么大容积的包，有dp[j]种方法
+//     dp := make([]int, bagSize+1)
+//     // 表示从前0个数字中选出若干个数字使得其和为0的方案数为1，即「空集合」不选任何数字即可得到0。
+//     dp[0] = 1
+//     for i := 0; i < n; i++ {
+//         for j := bagSize; j >= nums[i]; j-- {
+//             dp[j] += dp[j-nums[i]]
+//         }
+//     }
+//     return dp[bagSize]
+// }
+// func abs(a int) int {
+//     if a >= 0 {
+//         return a
+//     }
+//     return -a
+// }
+
+// ========================================
+// 回溯解法
+// def backtrack(nums, i):
+//     if i == len(nums):
+//         if 达到 target:
+//             result += 1
+//         return
+    
+//     for op in { +1, -1 }:
+//         选择 op * nums[i]
+//         # 穷举 nums[i + 1] 的选择
+//         backtrack(nums, i + 1)
+//         撤销选择
 func findTargetSumWays(nums []int, target int) int {
-    // left - right = target
-    // left + right = sum
-    // left = (target + sum)/2
-    sum, n := 0, len(nums)
-    for _, v := range nums {
-        sum += v
-    }
-    if (target + sum) % 2 == 1 || abs(target) > sum {
-        return 0
-    }
-    bagSize := (target + sum)/2
-    // dp[j] 表示：填满j（包括j）这么大容积的包，有dp[j]种方法
-    dp := make([]int, bagSize+1)
-    // 表示从前0个数字中选出若干个数字使得其和为0的方案数为1，即「空集合」不选任何数字即可得到0。
-    dp[0] = 1
-    for i := 0; i < n; i++ {
-        for j := bagSize; j >= nums[i]; j-- {
-            dp[j] += dp[j-nums[i]]
+    res := 0
+    var backtrack func(i, sum int)
+    backtrack = func(i, sum int) {
+        if i == len(nums) {
+            if sum == target {
+                res++
+            }
+            return
         }
+        // 遍历所有选择：+nums[i]、-nums[i]
+        backtrack(i+1, sum+nums[i])
+        backtrack(i+1, sum-nums[i])
     }
-    return dp[bagSize]
-}
-func abs(a int) int {
-    if a >= 0 {
-        return a
-    }
-    return -a
+    backtrack(0, 0)
+    return res
 }
 // @lc code=end
 
