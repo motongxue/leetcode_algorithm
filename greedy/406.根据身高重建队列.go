70ecc4ac45b17c0c8e10dfa479ab3f10a6d39ab8
motongxue <393888412@qq.com>
2023-08-12 15:20:07 +0800

    greedy：docs，重写406，实现指定位置插入

--- greedy/406.根据身高重建队列.go
+++ greedy/406.根据身高重建队列.go
@@ -23,5 +23,38 @@ func reconstructQueue(people [][]int) [][]int {
 	// }
 	return people
 }
+
+// ================================================================================
+// 重写
+// 采用插入排序的思想，注意，这里的插入排序是指将元素按照ki进行插入到指定位置，而不是在原数组位置上进行排序
+func reconstructQueue(people [][]int) [][]int {
+	// 按身高hi进行降序排序，并且需要对ki进行升序排序
+	sort.Slice(people, func(a, b int) bool {
+		if people[a][0] == people[b][0] {
+			return people[a][1] < people[b][1]
+		}
+		return people[a][0] > people[b][0]
+	})
+	// 初始化res返回结果
+	n := len(people)
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = make([]int, 2)
+	}
+	// 插入排序
+	for i := 0; i < n; i++ {
+		insert(people[i], res)
+	}
+	return res
+}
+func insert(v []int, res [][]int) {
+	n := len(res)
+	i := n - 1
+	for i = n - 1; i > v[1]; i-- {
+		res[i] = res[i-1]
+	}
+	res[i] = v
+}
+
 // @lc code=end
 
