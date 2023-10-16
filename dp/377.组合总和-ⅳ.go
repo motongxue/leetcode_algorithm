4338c6f643effb21f33a4b51f67232b01537d01d
motongxue <393888412@qq.com>
2023-10-16 13:26:41 +0800

    dp：[377] 组合总和 Ⅳ

---
+++ dp/377.组合总和-ⅳ.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=377 lang=golang
+ *
+ * [377] 组合总和 Ⅳ
+ */
+
+// @lc code=start
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+	// 由于是排列数，所以需要先遍历背包，再遍历物品
+	for j := 0; j <= target; j++ {
+		for i := 0; i < len(nums); i++ {
+			if j >= nums[i] {
+				dp[j] += dp[j-nums[i]]
+			}
+		}
+	}
+	return dp[target]
+}
+
+// @lc code=end
+
